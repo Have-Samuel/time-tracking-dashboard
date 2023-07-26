489cc299c9b031f8e381bd4cd53cf5853624f8e0
Have Samuel <samiehave9@gmail.com>
2023-07-27 00:55:45 +0300

    Uncomment the code

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 /* eslint-disable no-use-before-define */
 /* eslint-disable array-callback-return */
-// const infoCards = document.querySelector('.changer');
-// infoCards.append(cardInfo());
-// // Fetching the json API
-// fetch('http://localhost:3000/user')
-//   // Getting the data from database
-//   // And fast converting it into json
-//   .then((res) => res.json())
-//   // iterating the data and getting information from the data
-//   .then((json) => {
-//     json.map(() => {
+const infoCards = document.querySelector('.changer');
+infoCards.append(cardInfo());
+// Fetching the json API
+fetch('http://localhost:3000/user')
+  // Getting the data from database
+  // And fast converting it into json
+  .then((res) => res.json())
+  // iterating the data and getting information from the data
+  .then((json) => {
+    json.map(() => {
 
-//     });
-//   });
+    });
+  });
 
-// // Showing the data in the DOM
-// function cardInfo() {
-//   const cardShower = document.createElement('span');
-//   cardShower.innerHTML = `
+// Showing the data in the DOM
+function cardInfo() {
+  const cardShower = document.createElement('span');
+  cardShower.innerHTML = `
 
-//   `;
-//   return cardShower;
-// }
-// cardInfo();
+  `;
+  return cardShower;
+}
+cardInfo();
