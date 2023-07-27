132a428718171de40f117154a5447aa141c766e6
Have Samuel <samiehave9@gmail.com>
2023-07-27 23:44:02 +0300

    Lopping over all the cards

--- app.js
+++ app.js
@@ -10,28 +10,16 @@ const pervData = document.querySelector('.week-1');
 fetch('http://localhost:3000/user')
   // Getting the data from database
   // And fast converting it into json
-  .then((response) => response.json())
-  .then((data) => {
-    dailyStats.addEventListener('click', () => {
-      console.log(data);
-      // Looping through the data
-      data.map((item) => {
-        if (item.title === 'work') {
-          cardNo.innerHTML = `
-          <div class="card">
-            <div class="card-header">
-              <h3 class="card-title">Work</h3>
-              <div class="card-icon">
-                <img src="./images/icon-work.svg" alt="Work Icon" />
-              </div>
-            </div>
-            <div class="card-content">
-              <h1 class="card-time">${item.timeframes.daily.current}hrs</h1>
-              <p class="card-previous">Yesterday - ${item.timeframes.daily.previous}hrs</p>
-            </div>
-          </div>
-          `;
-        }
-      });
-    });
-  });
+  .then((res) => {
+    return res.json();
+  })
+  .then((cards) => {
+    const sectionCards = document.querySelector('#card-section');
+
+    let display = '';
+    for (let card of cards) {
+      display += `
+      
+      `;
+    }
+  })
