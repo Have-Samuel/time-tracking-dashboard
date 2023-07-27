c4e52b59871bbfb6508d91de3f00b2d8f02ab63c
Have Samuel <samiehave9@gmail.com>
2023-07-27 20:05:47 +0300

    Creating the daily var

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 /* eslint-disable no-use-before-define */
 /* eslint-disable array-callback-return */
-const infoCards = document.querySelector('.changer');
-infoCards.append(cardInfo());
-// Fetching the json API
-fetch('http://localhost:3000/user')
-  // Getting the data from database
-  // And fast converting it into json
-  .then((res) => res.json())
-  // iterating the data and getting information from the data
-  .then((json) => {
-    json.map(() => {
 
-    });
-  });
+// Fetching daily data from db.json
+const dailyStats = document.querySelector('.daily-data');
 
-// Showing the data in the DOM
+// infoCards.append(cardInfo());
+// // Fetching the json API
+// fetch('http://localhost:3000/user')
+//   // Getting the data from database
+//   // And fast converting it into json
+//   .then((res) => res.json())
+//   // iterating the data and getting information from the data
+//   .then((json) => {
+//     json.map(() => {
 
+//     });
+//   });
+
+// // Showing the data in the DOM
+// function cardInfo() {
+//   const cardShower = document.createElement('span');
+//   cardShower.innerHTML = `
+
+//   `;
+//   return cardShower;
+// }
+// cardInfo();
