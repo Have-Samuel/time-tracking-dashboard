532097420702741cf58c0495a8c6e67765fa370f
Have Samuel <samiehave9@gmail.com>
2023-07-30 04:30:36 +0300

    Displaying second card data

--- app.js
+++ app.js
@@ -14,26 +14,26 @@ fetch('http://localhost:3000/user')
   // And fast converting it into json
   .then((res) => res.json())
   // Getting the current and previous hours for title "work"
-  .then((cards) => {
+  .then((user) => {
     const title = '';
     // add an event to daily so that when you click on it, it will show the daily data
     // Card One
     dailyStats.addEventListener('click', () => {
-      console.log(cards[0].timeframes.daily.current);
-      console.log(cards[0].timeframes.daily.previous);
-      if (title === 'Work') {
-        cardNo.textContent = `${cards[0].timeframes.daily.current}hrs`;
-        prevData.textContent = `Yesterday - ${cards[0].timeframes.daily.previous}hrs`;
-      }
-      if (title === 'Play') {
-        cardNo.textContent = `${cards[1].timeframes.daily.current}hrs`;
-        prevData.textContent = `Yesterday - ${cards[1].timeframes.daily.previous}hrs`;
-      } if (title === 'Study') {
-        cardNo.textContent = `${cards[2].timeframes.daily.current}hrs`;
-        prevData.textContent = `Yesterday - ${cards[2].timeframes.daily.previous}hrs`;
-      }
-      // cardNo.textContent = `${cards[0].timeframes.daily.current}hrs`;
-      // prevData.textContent = `Yesterday - ${cards[0].timeframes.daily.previous}hrs`;
+      console.log(user[0].timeframes.daily.current);
+      console.log(user[0].timeframes.daily.previous);
+      // if (title === 'Work') {
+      //   cardNo.textContent = `${user[0].timeframes.daily.current}hrs`;
+      //   prevData.textContent = `Yesterday - ${user[0].timeframes.daily.previous}hrs`;
+      // }
+      // if (title === 'Play') {
+      //   cardNo.textContent = `${user[1].timeframes.daily.current}hrs`;
+      //   prevData.textContent = `Yesterday - ${user[1].timeframes.daily.previous}hrs`;
+      // } if (title === 'Study') {
+      //   cardNo.textContent = `${user[2].timeframes.daily.current}hrs`;
+      //   prevData.textContent = `Yesterday - ${user[2].timeframes.daily.previous}hrs`;
+      // }
+      cardNo.textContent = `${user[0].timeframes.daily.current}hrs`;
+      prevData.textContent = `Yesterday - ${user[0].timeframes.daily.previous}hrs`;
     });
     // weeklyStats.addEventListener('click', () => {
     //   console.log(cards[0].timeframes.weekly.current);
@@ -49,10 +49,10 @@ fetch('http://localhost:3000/user')
     // });
     // Card Two
     // dailyStats.addEventListener('click', () => {
-    //   console.log(cards[1].timeframes.daily.current);
-    //   console.log(cards[1].timeframes.daily.previous);
-    //   cardNo.textContent = `${cards[1].timeframes.daily.current}hrs`;
-    //   prevData.textContent = `Yesterday - ${cards[1].timeframes.daily.previous}hrs`;
+    //   console.log(user[1].timeframes.daily.current);
+    //   console.log(user[1].timeframes.daily.previous);
+    //   cardNo.textContent = `${user[1].timeframes.daily.current}hrs`;
+    //   prevData.textContent = `Yesterday - ${user[1].timeframes.daily.previous}hrs`;
     // });
     // weeklyStats.addEventListener('click', () => {
     //   console.log(cards[0].timeframes.weekly.current);
