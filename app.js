a524c88480edbee8b0f6fc1513f75b2a7eef6808
Have Samuel <samiehave9@gmail.com>
2023-07-31 03:31:58 +0300

    Removing the unnecessary code

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-use-before-define */
-/* eslint-disable array-callback-return */
-
 // Fetching daily data from db.json
 const dailyStats = document.querySelector('.daily-data');
 const weeklyStats = document.querySelector('.weekly-data');
@@ -28,80 +24,23 @@ const user = [
     },
   },
 ];
-// Fetching the json API
-fetch('http://localhost:3000/user')
-  // Getting the data from database
-  // And fast converting it into json
-  .then((res) => res.json())
-  // Getting the current and previous hours for title "work"
-  .then((user) => {
-    const title = '';
-    // add an event to daily so that when you click on it, it will show the daily data
-    // Card One
-    user.forEach((user) => {
-      dailyStats.addEventListener('click', () => {
-        console.log(user.timeframes.daily.current);
-        console.log(user.timeframes.daily.previous);
-        cardNo.textContent = `${user.timeframes.daily.current}hrs`;
-        prevData.textContent = `Yesterday - ${user.timeframes.daily.previous}hrs`;
-      });
-      weeklyStats.addEventListener('click', () => {
-        console.log(user.timeframes.weekly.current);
-        console.log(user.timeframes.weekly.previous);
-        cardNo.textContent = `${user.timeframes.weekly.current}hrs`;
-        prevData.textContent = `Yesterday - ${user.timeframes.weekly.previous}hrs`;
-      });
-      monthlyStats.addEventListener('click', () => {
-        console.log(user.timeframes.monthly.current);
-        console.log(user.timeframes.monthly.previous);
-        cardNo.textContent = `${user.timeframes.monthly.current}hrs`;
-        prevData.textContent = `Yesterday - ${user.timeframes.monthly.previous}hrs`;
-      });
-    });
-    // dailyStats.addEventListener('click', () => {
-    //   console.log(user[0].timeframes.daily.current);
-    //   // console.log(user[0].timeframes.daily.current);
-    //   console.log(user[0].timeframes.daily.previous);
-    //   cardNo.textContent = `${user[0].timeframes.daily.current}hrs`;
-    //   prevData.textContent = `Yesterday - ${user[0].timeframes.daily.previous}hrs`;
-    // });
-    // weeklyStats.addEventListener('click', () => {
-    //   console.log(user[0].timeframes.weekly.current);
-    //   console.log(user[0].timeframes.weekly.previous);
-    //   cardNo.textContent = `${user[0].timeframes.weekly.current}hrs`;
-    //   prevData.textContent = `Yesterday - ${user[0].timeframes.weekly.previous}hrs`;
-    // });
-    // monthlyStats.addEventListener('click', () => {
-    //   console.log(user[0].timeframes.monthly.current);
-    //   console.log(user[0].timeframes.monthly.previous);
-    //   cardNo.textContent = `${user[0].timeframes.monthly.current}hrs`;
-    //   prevData.textContent = `Yesterday - ${user[0].timeframes.monthly.previous}hrs`;
-    // });
-    // Card Two
-    // dailyStats.addEventListener('click', () => {
-    //   console.log(user[1].timeframes.daily.current);
-    //   console.log(user[1].timeframes.daily.previous);
-    //   cardNo.textContent = `${user[1].timeframes.daily.current}hrs`;
-    //   prevData.textContent = `Yesterday - ${user[1].timeframes.daily.previous}hrs`;
-    // });
-    // weeklyStats.addEventListener('click', () => {
-    //   console.log(cards[0].timeframes.weekly.current);
-    //   console.log(cards[0].timeframes.weekly.previous);
-    //   cardNo.textContent = `${cards[0].timeframes.weekly.current}hrs`;
-    //   prevData.textContent = `Yesterday - ${cards[0].timeframes.weekly.previous}hrs`;
-    // });
-    // monthlyStats.addEventListener('click', () => {
-    //   console.log(cards[0].timeframes.monthly.current);
-    //   console.log(cards[0].timeframes.monthly.previous);
-    //   cardNo.textContent = `${cards[0].timeframes.monthly.current}hrs`;
-    //   prevData.textContent = `Yesterday - ${cards[0].timeframes.monthly.previous}hrs`;
-    // });
-
-    // Card Three
-    // dailyStats.addEventListener('click', () => {
-    //   console.log(cards[2].timeframes.daily.current);
-    //   console.log(cards[2].timeframes.daily.previous);
-    //   cardNo.textContent = `${cards[2].timeframes.daily.current}hrs`;
-    //   prevData.textContent = `Yesterday - ${cards[2].timeframes.daily.previous}hrs`;
-    // });
+user.forEach((user) => {
+  dailyStats.addEventListener('click', () => {
+    console.log(user.timeframes.daily.current);
+    console.log(user.timeframes.daily.previous);
+    cardNo.textContent = `${user.timeframes.daily.current}hrs`;
+    prevData.textContent = `Yesterday - ${user.timeframes.daily.previous}hrs`;
   });
+  weeklyStats.addEventListener('click', () => {
+    console.log(user.timeframes.weekly.current);
+    console.log(user.timeframes.weekly.previous);
+    cardNo.textContent = `${user.timeframes.weekly.current}hrs`;
+    prevData.textContent = `Yesterday - ${user.timeframes.weekly.previous}hrs`;
+  });
+  monthlyStats.addEventListener('click', () => {
+    console.log(user.timeframes.monthly.current);
+    console.log(user.timeframes.monthly.previous);
+    cardNo.textContent = `${user.timeframes.monthly.current}hrs`;
+    prevData.textContent = `Yesterday - ${user.timeframes.monthly.previous}hrs`;
+  });
+});
